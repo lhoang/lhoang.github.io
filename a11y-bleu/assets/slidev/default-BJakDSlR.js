bc787fab4a09588d11482c45f90e200df7905042
Lang Hoang <hoanglang@gmail.com>
2024-11-14 16:50:40 +0100

    last update a11y talk

---
+++ a11y-bleu/assets/slidev/default-BJakDSlR.js
@@ -0,0 +1,1 @@
+import{o,b as t,f as s}from"../modules/vue-vy38_FvO.js";import{_ as r}from"../index-DJcAis7c.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
