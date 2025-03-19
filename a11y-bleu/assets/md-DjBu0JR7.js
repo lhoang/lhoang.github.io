cf418e5a915df00e50b751945868b25b4caad963
Lang Hoang <hoanglang@gmail.com>
2025-03-19 23:04:24 +0100

    update a11y talk - M. guiz

--- a11y-bleu/assets/md-DjBu0JR7.js
+++
@@ -1,7 +0,0 @@
-import{_ as i}from"./slidev/VClick-BCdGQOIq.js";import{o as l,c as u,k as n,e as t,l as m,g as d,q as p,s as c,A as s}from"./modules/vue-vy38_FvO.js";import{I as f}from"./slidev/center-C_RadXbQ.js";import{u as _,f as v}from"./slidev/context-CG6GGvBo.js";import"./index-DJcAis7c.js";import"./modules/shiki-BrNjSUz5.js";const q={__name:"slides.md__slidev_33",setup(b){const{$slidev:g,$nav:k,$clicksContext:o,$clicks:x,$page:$,$renderContext:C,$frontmatter:r}=_();return o.setup(),(L,e)=>{const a=i;return l(),u(f,p(c(s(v)(s(r),32))),{default:n(()=>[e[1]||(e[1]=t("p",null,"Taux actualisé Novembre 2024 : 62% - Partiellement conforme",-1)),e[2]||(e[2]=t("p",null,"Mais il reste encore beaucoup de choses à faire pour avoir 100%.",-1)),e[3]||(e[3]=t("p",null,"On aimerait faire des tests utilisateurs avec des personnes handicapées.",-1)),m(a,null,{default:n(()=>e[0]||(e[0]=[t("h3",null,"Loi de Goodhart :",-1),t("em",null," Lorsqu'une mesure devient un objectif, elle cesse d'être une bonne mesure ",-1)])),_:1}),d(`
--La déclaration de conformité est en cours de rédaction.<br>
-- Le schéma pluriannuel est également en cours de rédaction (atelier design thinking).
-
-- Une note est valable à un instant T.
-- Un site noté 100% conforme peut ne pas être accessible.
-`)]),_:1},16)}}};export{q as default};
